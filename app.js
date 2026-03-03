ec4c6c4fee42947b0cc3b3cb4615c94702053852
patelyashwanth78-ship-it <patelyashwanth78@gmail.com>
2026-03-03 22:28:14 +0530

    Update image filename to include extension

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const products = [
     price: 499,
     popularity: 92,
     createdAt: "2026-02-19",
-    image: "aot1",
+    image: "aot1.jpeg",
     description: "Sharp tailored fit with wrinkle-resistant finish, ideal for office and events.",
     sizes: ["M", "L", "XL"],
     rating: 4.7
